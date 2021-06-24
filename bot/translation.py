8206af115caff43c604416610f143dbf8624d6b1
reena0307 <71924864+reena0307@users.noreply.github.com>
2021-06-24 12:09:21 +0530

    Add files via upload

---
+++ bot/translation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+class Translation(object):
+    
+    START_TEXT = """<b>Hey {}!!</b>
+<i>CineCube Movie Bot
+</i>"""    
+    
+    HELP_TEXT = """
+<b><i><u>How To Use Me ?</u></i></b>
+
+<i>
+-> Just Make a Clone and Enjoy
+</i>
+     
+"""
+    
+    ABOUT_TEXT = """
+🤖 𝗠𝗬 𝗡𝗔𝗠𝗘 : <a href="https://t.me/CineCubeBot">CineCube Movie Bot</a>
+"""
